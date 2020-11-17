82be36ca515c8dfb1626c4f26b0fd3807662df30
Julio Alberto <julioj1081@hotmail.com>
2020-11-17 15:29:50 -0600

    Efecto parallax galeria

---
+++ js/app.js
@@ -0,0 +1,22 @@
+// external js: flickity.pkgd.js
+
+var carousel = document.querySelector('.carousel');
+var flkty = new Flickity( carousel, {
+  imagesLoaded: true,
+  percentPosition: true,
+});
+
+var imgs = carousel.querySelectorAll('.carousel-cell img');
+// get transform property
+var docStyle = document.documentElement.style;
+var transformProp = typeof docStyle.transform == 'string' ?
+  'transform' : 'WebkitTransform';
+
+flkty.on( 'scroll', function() {
+  flkty.slides.forEach( function( slide, i ) {
+    var img = imgs[i];
+    var x = ( slide.target + flkty.x ) * -1/3;
+    img.style[ transformProp ] = 'translateX(' + x  + 'px) scale(1.1)';
+  });
+});
+
